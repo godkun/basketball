b65dbc17c0674c654cb27c1cb5457fdd49255554
godkun <godkun666@126.com>
2022-10-16 13:46:03 +0800

    docs: 完善文档

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,43 @@
 module.exports = {
   title: 'ikun的篮球空间',
-  description: 'just like basketball',
+  description: 'love basketball',
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
   themeConfig: {
     repo: 'godkun/basketball',
     logo: '/logo.svg',
     docsDir: 'docs',
-    nav: [{ text: 'github', link: 'https://github.com/godkun/basketball' }],
+    nav: [{ text: 'Guide', link: '/guide/教程视频-屈髋' }],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/godkun/basketball' }
+    ],
     sidebar: [
       {
         text: '教程视频',
         items: [
           { text: '屈髋', link: '/guide/教程视频-屈髋' },
           { text: '防守断球', link: '/guide/教程视频-防守断球' }
-        ],
+        ]
       },
       {
         text: '训练方案',
         items: [{ text: '投篮', link: '/guide/训练方案-投篮' }]
+      },
+      {
+        text: '篮球装备',
+        items: [{ text: '篮球', link: '/guide/篮球装备-篮球' }]
+      },
+      {
+        text: 'ikun篮球空间',
+        items: [
+          { text: '首页', link: '/guide/ikun篮球空间-首页' },
+          { text: '联系作者', link: '/guide/ikun篮球空间-联系作者' }
+        ]
       }
     ],
 
     editLink: {
       pattern: 'https://github.com/godkun/basketball/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
+      text: '点击编辑此页'
     }
   }
 }
--- docs/index.md
+++ docs/index.md
@@ -3,7 +3,7 @@ layout: home
 
 hero:
   name: ikun的篮球空间
-  tagline: 欢迎提pr或者issues一起完善
+  tagline: 欢迎一起完善
   image:
     src: /logo.svg
     alt: Basketball
@@ -15,11 +15,14 @@ hero:
 features:
   - icon: 🏀
     title: 教程视频
-    details: 收集的优秀教程短视频
+    # details: 收集的优秀教程短视频
   - icon: 🏀
     title: 训练方案
-    details: 各种训练方案
+    # details: 训练方案
   - icon: 🏀
     title: 篮球装备
-    details: 一些装备
+    # details: 一些装备
+  - icon: 🏀
+    title: ikun篮球空间
+    # details: 
 ---
