6088e668631ecd8040d852b39024b6827fc2cd4d
godkun <godkun666@126.com>
2022-10-16 12:40:09 +0800

    feat: initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'ikun的篮球空间',
+  description: 'just like basketball',
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+  themeConfig: {
+    repo: 'godkun/basketball',
+    logo: '/logo.svg',
+    docsDir: 'docs',
+    nav: [{ text: 'github', link: 'https://github.com/godkun/basketball' }],
+    sidebar: [
+      {
+        text: '教程视频',
+        items: [{ text: '屈髋', link: '/guide/教程视频-屈髋' }]
+      },
+      {
+        text: '训练方案',
+        items: [{ text: '投篮', link: '/guide/训练方案-投篮' }]
+      }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/godkun/basketball/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    }
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+layout: home
+
+hero:
+  name: ikun的篮球空间
+  tagline: 欢迎提pr或者issues一起完善
+  image:
+    src: /logo.svg
+    alt: Basketball
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/教程视频-屈髋
+
+features:
+  - icon: 🏀
+    title: 教程视频
+    details: 收集的优秀教程短视频
+  - icon: 🏀
+    title: 训练方案
+    details: 各种训练方案
+  - icon: 🏀
+    title: 篮球装备
+    details: 一些装备
+---
